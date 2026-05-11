da886884ffb01736cabfa236937f44bebad14005
GTU9 <107167326+GTU9@users.noreply.github.com>
2026-05-11 23:43:18 +0900

    Update index.js

--- senials_frontend/src/index.js
+++ senials_frontend/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import {Provider} from "react-redux"
 import store from './redux/store.js'
+import axios from 'axios';
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
